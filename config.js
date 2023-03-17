052da09455db8459c03f46e26eeba6bab71716e5
Mary <ngangamary078@gmail.com>
2023-03-17 09:12:07 +0300

    added some insertions on backened

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports ={
+
+    mongoDB_URI: 'mongodb+srv://ngangamary078:Kimberly254@moringaproductapp.phwmdqp.mongodb.net/test'
+ 
+ }
---
+++ server.js
@@ -0,0 +1,95 @@
+const express = require('express');
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
+
+
+
+const productRoute = require('./routes/productRoute');
+
+
+
+// Initializing express
+
+const app = express()
+
+
+
+// Body parser middleware
+
+app.use(express.json())
+
+
+
+//DB config
+
+
+
+const MONGODB_URI = process.env.MONGODB_URI ||
+
+
+
+    require('./config').mongoDB_URI
+
+
+
+// Connect to MongoDB
+
+
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+
+
+// Check Connection
+
+
+
+let db = mongoose.connection;
+
+
+
+db.once('open', () => {
+
+
+
+    console.log('Database connected successfully')
+
+
+
+})
+
+
+
+// Check for DB Errors
+
+
+
+db.on('error', (error) => {
+
+
+
+    console.log(error);
+
+
+
+    // Use Routes
+
+
+
+})
+
+
+
+
+
+// Define the PORT
+
+const PORT = process.env.PORT || 5000
+app.use('/products', productRoute)  //new code
+
+
+app.listen(PORT, () => {
+
+    console.log(`Server listening on port ${PORT}`)
+
+})
